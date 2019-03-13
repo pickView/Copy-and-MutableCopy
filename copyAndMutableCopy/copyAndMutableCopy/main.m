cb721414361ba1677c961f1d1d45c29c4f558839
allenChou <changxu.zhou@ydcfo.com>
2019-03-13 15:09:47 +0800

    feature：新增验证代码

--- copyAndMutableCopy/copyAndMutableCopy/main.m
+++ copyAndMutableCopy/copyAndMutableCopy/main.m
@@ -11,21 +11,31 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSMutableArray *str = [NSMutableArray arrayWithObjects:@"this is test", nil];
-        NSMutableArray *strCopy = [str copy];
-        NSMutableArray *strMutableCopy = [str copy];
-        NSLog(@"str:%p",str);
-        NSLog(@"strCopy:%p",strCopy);
-        NSLog(@"strMutableCopy:%p",strMutableCopy);
+//        NSMutableArray *str = [NSMutableArray arrayWithObjects:@"this is test", nil];
+//        NSMutableArray *strCopy = [str copy];
+//        NSMutableArray *strMutableCopy = [str copy];
+//        NSLog(@"str:%p",str);
+//        NSLog(@"strCopy:%p",strCopy);
+//        NSLog(@"strMutableCopy:%p",strMutableCopy);
         
-        
-//        NSMutableString *mstr = [NSMutableString stringWithFormat:@"nice!"];
+//        NSString *mstr = [NSString stringWithFormat:@"nice!"];
 //        NSString *mstrCopy = [mstr copy];
 //        NSString *mstrMutableCopy = [mstr mutableCopy];
 //        NSLog(@"mstr:%p",mstr);
 //        NSLog(@"mstrCopy:%p",mstrCopy);
 //        NSLog(@"mstrMutableCopy:%p",mstrMutableCopy);
+//        mstr = @"1234";
+//        NSLog(@"%@-%@-%@",mstr,mstrCopy,mstrMutableCopy);
         
+        NSMutableString *mstr = [NSMutableString stringWithFormat:@"nice!"];
+        NSString *mstrCopy = [mstr copy];
+        NSString *mstrMutableCopy = [mstr mutableCopy];
+        NSLog(@"mstr:%p",mstr);
+        NSLog(@"mstrCopy:%p",mstrCopy);
+        NSLog(@"mstrMutableCopy:%p",mstrMutableCopy);
+        mstr = [NSMutableString stringWithFormat:@"12345"];
+        NSLog(@"%@-%@-%@",mstr,mstrCopy,mstrMutableCopy);
+
 //        NSMutableArray *arr = [NSMutableArray arrayWithObjects:@"1",@"2",@"3", nil];
 //        Warrior *w = [Warrior new];
 //        w.skills = arr;
